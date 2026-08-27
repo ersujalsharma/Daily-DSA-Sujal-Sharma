20fa5673ae903c4f28c98182b7e088e5a3e0ec22
sujalsharma1 <sujalsharma1@bitbucket.com>
2026-08-27 16:36:46 +0530

    Question 13 added

---
+++ Hashing/Hashing_13_CSUMQ_Cumulative_Sum_Query.java
@@ -0,0 +1,24 @@
+package Hashing;
+
+import java.util.*;
+
+/*
+
+13. CSUMQ - Cumulative Sum Query
+
+William Macfarlane wants to look at an array.
+
+You are given a list of N numbers and Q queries. Each query is specified by two numbers i and j; the answer to each query is the sum of every number between the range [i, j] (inclusive).
+
+Note: the query ranges are specified using 0-based indexing.
+
+*/
+public class Hashing_13_CSUMQ_Cumulative_Sum_Query {
+    
+    public static void main(String[] args) {
+        
+    }
+
+    
+
+}
