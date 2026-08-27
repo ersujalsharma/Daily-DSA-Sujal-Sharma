c81ab198f1bddddae6890e69ecd5ee8bf4f407a9
sujalsharma1 <sujalsharma1@bitbucket.com>
2026-08-27 14:33:20 +0530

    Valid Anagrams

---
+++ Hashing/Hashing_10.java
@@ -0,0 +1,33 @@
+package Hashing;
+
+/*
+
+10. Valid Anagrams
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+*/
+
+public class Hashing_10 {
+    public static void main(String[] args) {
+        String s = "anagram",t = "nagaram";
+        boolean val = checkAnagram(s,t);
+        System.out.println("Both are Anagrams : "+val);
+    }
+
+    private static boolean checkAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+
+        int freq[] = new int[26];
+
+        for(char c : s.toCharArray()){
+            freq[c-'a']++;
+        }
+        for(char c : t.toCharArray()){
+            freq[c-'a']--;
+        }
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]!=0) return false;
+        }
+        return true;
+    }
+    
+}
