c6e615a2b04341ef80e5decfcd99b99d95bf184a
sujalsharma1 <sujalsharma1@bitbucket.com>
2026-08-28 16:40:08 +0530

    spoj question 13

--- Hashing/Hashing_13_CSUMQ_Cumulative_Sum_Query.java
+++ Hashing/Hashing_13_CSUMQ_Cumulative_Sum_Query.java
@@ -21,12 +21,24 @@ public class Hashing_13_CSUMQ_Cumulative_Sum_Query {
         int q = 3;
         int query[][] = {{1,1},{1,2},{0,2}};
         int ans[] = cum_sum_query(n,arr,q,query);
-        Arrays.toString(ans);
+        System.out.println(Arrays.toString(ans));
     }
 
     private static int[] cum_sum_query(int n, int[] arr, int q, int[][] query) {
-        // TODO Auto-generated method stub
-        
+        int prefixSum[] = new int[n];
+        prefixSum[0] = arr[0];
+        for(int i=1;i<arr.length;i++){
+            prefixSum[i] = arr[i] + prefixSum[i-1];
+        }
+        int ans[] = new int[q];
+        int index = 0;
+        for(int qu[] : query){
+            int from = qu[0];
+            int to = qu[1];
+            int sum = prefixSum[to] - (from!=0?prefixSum[from-1]:0);
+            ans[index++] = sum;
+        }
+        return ans;
     }
 
 }
