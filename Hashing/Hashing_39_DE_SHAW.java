28d0f7e26ddeed593d7cac22958b9f9051c4bf2d
sujalsharma1 <sujalsharma1@bitbucket.com>
2026-09-18 17:28:27 +0530

    Hashing_39

--- Hashing/Hashing_39_DE_SHAW.java
+++ Hashing/Hashing_39_DE_SHAW.java
@@ -19,12 +19,30 @@ public class Hashing_39_DE_SHAW {
         System.out.println("Answer is -> " + ans);
     }
 
-    private static int help(int[] arr, int sumVal) {
-        HashMap<Integer,Integer> hashmap = new HashMap<>();
-        int count = 0;
-        for(int i : arr){
-            hashmap.put(i,hashmap.getOrDefault(i, 0)+1);
+    private static int help(int[] a, int t) {
+        int answer = 0;
+        if (t % 2 == 0) {
+            long positive = t / 2;
+            long negative = -positive;
+ 
+            long c1 = 0, c2 = 0, c5 = 0, c8 = 0;
+ 
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] == positive) c1++;
+                if (a[i] == negative) c2++;
+ 
+                if (a[i] >= 0 && a[i] < positive) c5++;
+ 
+                if (a[i] < 0 && a[i] > negative) c8++;
+            }
+ 
+            answer += (c1 * (c1 - 1)) / 2;
+            answer += (c2 * (c2 - 1)) / 2;
+            answer += c1 * c5;
+            answer += c2 * c8;
+            answer += c1 * c8;
+            answer += c2 * c5;
         }
-        return count;
+        return answer;
     }
 }
